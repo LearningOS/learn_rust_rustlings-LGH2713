ccb8f78b43d1791f6c65d8118fe174df50144b4c
LGH2713 <72382960+LGH2713@users.noreply.github.com>
2022-10-23 08:51:58 +0000

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,8 +1,6 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
     Move(Point),
@@ -45,7 +43,7 @@ impl State {
             Message::ChangeColor(color) => self.change_color(color),
             Message::Echo(s) => self.echo(s),
             Message::Quit => self.quit(),
-            Message::Move(p) => self.move_position(p)
+            Message::Move(p) => self.move_position(p),
         }
     }
 }
